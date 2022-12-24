18c37043735ac5c68bf1ee43e02fb22431b273b5
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2022-12-24 14:38:08 +0100

    Auth

---
+++ src/features/auth/authApiSlice.js
@@ -0,0 +1,37 @@
+import { apiSlice } from "../../app/api/apiSlice";
+import { logOut } from "./authSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        login: builder.mutation({
+            query: credentials => ({
+                url: '/auth',
+                method: 'POST',
+                body: { ...credentials }
+            })
+        }),
+        sendLogout: builder.mutation({
+            query: () => ({
+                url: 'auth/logout',
+                method: 'POST',
+            }),
+            async onQueryStarted(args, {dispatch, queryFulfilled}) {
+                try {
+                    const {data} = await queryFulfilled;
+                    console.log(data);
+                    dispatch(logOut());
+                    setTimeout (() => {
+                        dispatch(apiSlice.util.resetApiState());
+                    }, 1000);
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        }),
+        
+    })
+});
+
+export const {
+    useLoginMutation
+} = authApiSlice;
